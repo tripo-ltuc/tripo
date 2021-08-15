25ecb28826d24fe8171f51976a3f75b7253550b8
GHADEER KHASAWNEH <GHADEER_KHASAWNEH@YAHOO.COM>
2021-08-15 11:46:18 +0300

    working on postcards

--- src/App.js
+++ src/App.js
@@ -1,30 +1,34 @@
-import React from 'react';
-import { withAuth0 } from '@auth0/auth0-react';
-import Home from './components/pages/Home';
-import Login from './components/Login';
-import Header from './components/Header';
-import Profile from './components/Profile';
-import Footer from './components/Footer';
-import './App.css';
-import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
+import React from "react";
+import { withAuth0 } from "@auth0/auth0-react";
+import Home from "./components/pages/Home";
+import Login from "./components/Login";
+import Header from "./components/Header";
+import Profile from "./components/Profile";
+import Footer from "./components/Footer";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import PostCards from "./PostCards";
 
 class App extends React.Component {
   render() {
     return (
       <>
-      <section>
-        <Router>
+        <section>
+          <Router>
             <Header />
             <Switch>
               <Route exact path="/">
-                {(this.props.auth0.isAuthenticated &&  <Home/> )}
-                {!this.props.auth0.isAuthenticated && (<Login/>)}
-                </Route>
+                {this.props.auth0.isAuthenticated && <Home />}
+                {!this.props.auth0.isAuthenticated && <Login />}
+              </Route>
               <Route exact path="/profile">
-                {this.props.auth0.isAuthenticated && (<Profile/>)}
+                {this.props.auth0.isAuthenticated && <Profile />}
+              </Route>
+              <Route exact path="/postCards">
+                <PostCards />
               </Route>
             </Switch>
-        </Router>
+          </Router>
         </section>
         <Footer />
       </>
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Navbar, Nav, Container } from "react-bootstrap";
 
 class Home extends React.Component {
   render() {
     return (
       <Container fluid>
-        
         <h1>HI</h1>
       </Container>
     );
