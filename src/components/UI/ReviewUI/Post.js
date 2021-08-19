6399029e3a2499b0095367f80e4cfdc1fb75c7f5
macRos-Alzoubi <macRos.alzoubi@gmail.com>
2021-08-19 09:17:06 +0300

    old data

--- src/components/UI/ReviewUI/Post.js
+++ src/components/UI/ReviewUI/Post.js
@@ -2,7 +2,7 @@ import { Component } from "react";
 import { Button, Card } from "react-bootstrap";
 import { withAuth0 } from "@auth0/auth0-react";
 import { Form, FloatingLabel } from "react-bootstrap";
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import './Styles/post.css';
 import Comment from "./Comment";
 
@@ -26,48 +26,52 @@ class Post extends Component{
         const {user} = this.props.auth0;
         return(
             <>
-                <Card className="card">
+               <div className="container">
+               <Card className="card">
                     <Card.Body className="main-body">
                         <Card.Title className="card-title">
                             <Card.Body className="title-body">
-                                <Card.Img className="mx-2 rounded-circle" style={{width: '5rem'}} src={this.props.item.userImg} alt="userImg"/>
-                                <Card.Text>{this.props.item.userName}</Card.Text>
+                                <Card.Img className="user-img" style={{width: '5rem'}} src={this.props.item.userImg} alt="userImg"/>
+                                <Card.Text className="user-name" >{this.props.item.userName}</Card.Text>
                             </Card.Body>
-                            <Card.Title className="mr-2">{this.props.item.cityName}</Card.Title>
+                            <Card.Title className="city-name">{this.props.item.cityName}</Card.Title>
                         </Card.Title>
                         <Card.Body>
                             <Card.Img src={this.props.item.cityImg}/>
-                            <Card.Text>{this.props.item.content}</Card.Text>
+                            <Card.Text className ="post-content" >{this.props.item.content}</Card.Text>
                         </Card.Body>
-                        <Card.Body className="d-flex">
-                            <i class="fas fa-heart mx-2"></i>{this.props.item.likes}
-                        </Card.Body>
-                        <Card.Footer>
+                        <Card.Body className="post-control-container">
+                            <Card.Body><i class="fas fa-heart mx-2"></i>{this.props.item.likes}</Card.Body>
                             {user.email === this.props.item.userEmail
-                            && <Card.Body>
-                                <Button onClick={() => this.props.deletePost(this.props.item._id)}
+                            && <Card.Body className="controls-body">
+                                <Button className="btn btn--delete" onClick={() => this.props.deletePost(this.props.item._id)}
                                 >Delete</Button>
                                 
-                                <Button onClick={() => this.props.updatePost(this.props.item._id, this.props.idx)}
+                                <Button className="btn btn--update" onClick={() => this.props.updatePost(this.props.item._id, this.props.idx)}
                                 >Edit</Button>
                             </Card.Body>}
+                        </Card.Body>
+                        <Card.Body>
                             <Card.Body>
                                 <Form onSubmit={(event) => this.commentHandler(event, this.props.item._id)}>
-                                    <FloatingLabel controlId="floatingTextarea2" label="Comments">
-                                        <Form.Control
-                                        as="textarea"
-                                        placeholder="Leave a comment here"
-                                        style={{ height: '100px' }}
-                                        name="userComment"
-                                        />
-                                    </FloatingLabel>
-                                    <Button type="submit">Add Comment</Button>
+                                    <Form.Group className="form-group">
+                                        <FloatingLabel controlId="floatingTextarea2" label="Leave A Comment">
+                                            <Form.Control
+                                            as="textarea"
+                                            placeholder="Leave a comment here"
+                                            name="userComment"
+                                            style={{height: '60rem'}}
+                                            // className="comment-text"
+                                            />
+                                        </FloatingLabel>
+                                        <Button className="form-btn" type="submit">Add Comment</Button>
+                                    </Form.Group>
                                 </Form>
                             </Card.Body>
-                            {this.props.item.comments.length
+                            {this.props.item.comments.length !== 0
                             && <Card.Body 
                             className="d-flex flex-column">
-                                    {this.props.item.comments.length
+                                    {this.props.item.comments.length !== 0
                                      &&this.props.item.comments.map((comment, idx) => {
                                         
                                         return <Comment 
@@ -81,9 +85,10 @@ class Post extends Component{
                                                 />
                                     })}
                                </Card.Body>}
-                        </Card.Footer>
+                        </Card.Body>
                     </Card.Body>
                 </Card>
+               </div>
             </>
         );
     }
